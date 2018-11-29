5c9083010bf077b7ec5e5d0ab923cb867bc0ad14
Jan200101 <sentrycraft123@gmail.com>
2018-11-29 17:44:06 +0100

    Add logging utils

---
+++ cogs/utils/logger.py
@@ -0,0 +1,28 @@
+from datetime import date, datetime
+from sys import _getframe
+
+
+class Logger:
+
+    def __init__(self, name, *, dateform="%d/%m/%Y %H:%M"):
+        self.name = name
+        self.dateform = dateform
+
+    def write(self, content, type, function):
+        time = datetime.now().strftime(self.dateform)
+        with open("logs/{}.log".format(str(date.today())), "a") as file:
+            file.write("[{time}] {type} {name} {function}: {content}\n".format(
+                time=time, type=type, name=self.name, function=function, content=content))
+
+    def _add(self, type, content, *, display=False):
+        function = _getframe(1).f_code.co_name
+        self.write(content, type, function)
+        if display:
+            print("[{function}] {content}".format(
+                function=function, content=content))
+
+    def info(self, content):
+        self._add("INFO", content)
+
+    def warn(self, content):
+        self._add("WARNING", content, display=True)
