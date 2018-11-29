ff96039de961e3d3b53081c54fde1d3361038da3
Jan200101 <sentrycraft123@gmail.com>
2018-11-29 18:06:21 +0100

    [Logger] update stack tracing code

--- cogs/utils/logger.py
+++ cogs/utils/logger.py
@@ -14,15 +14,16 @@ class Logger:
             file.write("[{time}] {type} {name} {function}: {content}\n".format(
                 time=time, type=type, name=self.name, function=function, content=content))
 
-    def _add(self, type, content, *, display=False):
-        function = _getframe(1).f_code.co_name
+    def _add(self, type, content, function=None, *, display=False):
+        if not function:
+            function = _getframe(1).f_code.co_name
         self.write(content, type, function)
         if display:
             print("[{function}] {content}".format(
                 function=function, content=content))
 
     def info(self, content):
-        self._add("INFO", content)
+        self._add("INFO", content, _getframe(1).f_code.co_name)
 
     def warn(self, content):
-        self._add("WARNING", content, display=True)
+        self._add("WARNING", content, _getframe(1).f_code.co_name, display=True)
