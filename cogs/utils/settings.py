95e222eb0897223c58e643df8ba3c8f437a77935
Jan200101 <sentrycraft123@gmail.com>
2018-12-02 12:59:13 +0100

    [Settings] use custom dict object for config, create proper setter and getter functions, add preset setters and getters

--- cogs/utils/settings.py
+++ cogs/utils/settings.py
@@ -2,25 +2,32 @@ from discord import Guild
 from json import load, dump
 
 
+class config(dict):
+
+    def __missing__(self, key):
+        return self['DEFAULT']
+
+
 class Settings():
 
     def __init__(self):
         try:
-            self.settings = load(open("data/settings.json"))
+            self.settings = load(
+                open("data/settings.json"), object_hook=config)
         except (IOError, ValueError):
-            self.settings = {
-                "DEFAULT": {
+            self.settings = config({
+                "DEFAULT": config({
                     "PREFIX": ["!"],
                     "ADMIN_ROLE": "Admin",
                     "MODERATOR_ROLE": "Moderator",
-                }
-            }
+                })
+            })
 
-        self.default_settings = {
+        self.default_settings = config{
             "PREFIX": None,
             "ADMIN_ROLE": None,
             "MODERATOR_ROLE": None,
-        }
+        })
 
     def save(self):
         with open("data/settings.json", "w") as conf:
@@ -32,35 +39,54 @@ class Settings():
             self.save()
         return wrapper
 
+    def setvalue(self, keyid, key, value):
+        if not str(keyid) in self.settings:
+            self.settings[str(keyid)] = self.default_settings.copy()
+        self.settings[str(keyid)][key] = value
+
+    def getvalue(self, keyid, key):
+        value = self.settings[str(keyid)][key]
+        if not value:
+            value = self.settings["DEFAULT"][key]
+        return value
+
     @setter
     def setglobalsettings(self, prefix: list, admin: str = None, moderator: str = None):
-        self.settings['DEFAULT']["PREFIX"] = prefix
+        self.setvalue('DEFAULT', 'PREFIX', prefix)
         if admin:
-            self.settings['DEFAULT']["ADMIN_ROLE"] = admin
+            self.setvalue('DEFAULT', 'ADMIN_ROLE', admin)
         if moderator:
-            self.settings['DEFAULT']["MODERATOR_ROLE"] = moderator
+            self.setvalue('DEFAULT', 'MODERATOR_ROLE', moderator)
 
     @setter
     def setglobalprefix(self, prefix: list):
-        self.settings['DEFAULT']['PREFIX'] = prefix
+        self.setvalue('DEFAULT', 'PREFIX', prefix)
+
+    @setter
+    def setglobaladminrole(self, prefix: list):
+        self.setvalue('DEFAULT', 'ADMIN_ROLE', prefix)
+
+    @setter
+    def setglobalmoderatorrole(self, prefix: list):
+        self.setvalue('DEFAULT', 'MODERATOR_ROLE', prefix)
 
     @setter
     def setprefix(self, guild: Guild, prefix):
-        if not str(guild.id) in self.settings: self.settings[str(guild.id)] = self.default_settings.copy()
-        self.settings[str(guild.id)]['PREFIX'] = prefix
+        self.setvalue(guild.id, 'PREFIX', prefix)
+
+    @setter
+    def setadminrole(self, guild: Guild, prefix):
+        self.setvalue(guild.id, 'ADMIN_ROLE', prefix)
+
+    @setter
+    def setmoderatorrole(self, guild: Guild, prefix):
+        return self.setvalue(guild.id, 'MODERATOR_ROLE', prefix)
 
     def getprefix(self, guild: Guild = None) -> list:
-        if not guild or not str(guild.id) in self.settings or not self.settings[str(guild.id)]['PREFIX']:
-            return self.settings['DEFAULT']['PREFIX']
-        return self.settings[str(guild.id)]['PREFIX']
+        return self.getvalue(guild.id, 'PREFIX')
 
     def getadminrole(self, guild: Guild = None) -> str:
-        if not guild or not str(guild.id) in self.settings or not self.settings[str(guild.id)]['ADMIN_ROLE']:
-            return self.settings['DEFAULT']['ADMIN_ROLE']
-        return self.settings[str(guild.id)]['ADMIN_ROLE']
+        return self.getvalue(guild.id, 'ADMIN_ROLE')
 
     def getmoderatorrole(self, guild: Guild = None) -> str:
-        if not guild or not str(guild.id) in self.settings or not self.settings[str(guild.id)]['MODERATOR_ROLE']:
-            return self.settings['DEFAULT']['MODERATOR_ROLE']
-        return self.settings[str(guild.id)]['MODERATOR_ROLE']
-
+        return self.getvalue(guild.id, 'MODERATOR_ROLE')
