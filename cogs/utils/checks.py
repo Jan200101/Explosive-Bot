7a9e8135a710745a861b6179b8af4dabb5f82ec6
Jan200101 <sentrycraft123@gmail.com>
2018-12-03 21:47:55 +0100

    [Checks] cleanup

--- cogs/utils/checks.py
+++ cogs/utils/checks.py
@@ -1,16 +1,36 @@
 from discord.ext.commands import check
 
-def is_staff():
-    return any(is_moderator(), is_admin(), is_owner())
+
+def check_rolename(ctx, rolename):
+    if type(rolename) == str:
+        return rolename in [y.name for y in ctx.message.author.roles]
+    elif type(rolename) in (list, tuple, set):
+        for role in rolename:
+            if role in [y.name for y in ctx.message.author.roles]:
+                return True
+        return False
+
+
+def check_mod(ctx):
+    return check_rolename(ctx, ctx.bot.settings.getmoderatorrole())
+
+
+def check_admin(ctx):
+    return check_rolename(ctx, (ctx.bot.settings.getadminrole(), ctx.bot.settings.getmoderatorrole()))
+
+
+def check_owner(ctx):
+    return ctx.bot.owner == ctx.message.author
+
 
 def is_moderator():
-    return (lambda ctx: ctx.bot.settings.getmoderatorrole() in [y.name for y in ctx.message.author.roles])
+    return check(check_mod)
 
 def is_mod():
     return is_moderator()
 
 def is_admin():
-    return (lambda ctx: ctx.bot.settings.getadminrole() in [y.name for y in ctx.message.author.roles])
+    return check(check_admin)
 
 def is_owner():
-    return check((lambda ctx: ctx.bot.owner == ctx.message.author.id))
+    return check(check_owner)
