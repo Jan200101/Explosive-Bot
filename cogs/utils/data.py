6aba42d2b279741c4b728a887c6142548af363fb
Jan200101 <sentrycraft123@gmail.com>
2018-12-03 21:53:23 +0100

    [Utils] add custom data class

---
+++ cogs/utils/data.py
@@ -0,0 +1,23 @@
+from json import load, dump
+
+class Data(dict):
+
+    def __init__(self, path: str, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        with open(path) as file:
+            self.update(load(file))
+
+    def __setitem__(self, key, value):
+        super().__setitem__(str(key), value)
+        self.__save__()
+
+    def __delitem__(self, key):
+        super().__delitem__(str(key))
+        self.__save__()
+
+    def __getitem__(self, key):
+        return super().__getitem__(str(key))
+
+    def __save__(self):
+        with open(self.path, "w") as file:
+            dump(self, file)
